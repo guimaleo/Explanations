8b1f5131b3e176df6e6bfc5c3c8f6bfde2cdaaf3
Leonardo de Guimarães <128752196+guimaleo@users.noreply.github.com>
2023-10-11 03:09:57 +0100

    Bitwise function
    Added function to change to bit value of the values in the arrays

---
+++ membitaddress.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void	show_binary(int n, int bit)
+{
+	int	i;
+
+	i = bit - 1;
+	while (i >= 0)
+	{
+		printf("%i", (n >> i) & 1);
+		i--;
+	}
+}
+
+int	main()
+{
+	char	str[] = "Batata";
+	int	arr[] = {1, 2, 3, 4, 5, 6};
+
+	printf("For the str\n->%s\n\n", str);
+	printf("Hex address: %p		Decimal Address: %lu\n\n", str, (unsigned long)str);
+	int	i = 0;
+	while (str[i]) {
+		printf("str[%i]: %c --> %lu Binary: ", i, str[i], (unsigned long)&str[i]);
+		show_binary(str[i], (sizeof(char) * 8));
+		printf("\n");
+		i++;
+	}
+	printf("Variable size: %lu bytes\nString size: %lu\n", sizeof(char), sizeof(str));
+	i = 0;
+	printf("\n\nFor the array\n-> {1, 2, 3, 4, 5, 6}\n\n");
+	printf("Hex address: %p		Decimal Address: %lu\n\n", arr, (unsigned long)arr);
+	while (i < sizeof(arr) / sizeof(int))
+	{
+		printf("Arr[%i]: %i --> %lu Binary: ", i, arr[i], (unsigned long)&arr[i]);
+		show_binary(arr[i], (sizeof(int) * 8));
+		printf("\n");
+		i++;
+	}
+	printf("Variable size: %lu bytes\nArray size: %lu\n", sizeof(int), sizeof(arr));
+}
